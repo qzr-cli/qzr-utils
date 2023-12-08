e3b2a2a5e1aefbaf6adfd13629ec30271657f64c
Qzr <z5021996@vip.qq.com>
2023-12-08 16:20:19 +0800

    重新打包,压缩

--- dist/modules/Core/modules/core.d.ts
+++ dist/modules/Core/modules/core.d.ts
@@ -16,13 +16,22 @@ declare class Core {
      * @param {*} item 传入需要判断的变量
      * @return {string} string/object/array/number/boolean/undefined/null/symbol
      */
-    static checkType(item: any): string;
+    static checkType(item: any): 'string' | 'object' | 'array' | 'number' | 'boolean' | 'undefined' | 'null' | 'symbol';
     /**
      * 生成随机字符串
      * @param {number} len 位数
      * @returns {string} 随机字符串
      */
     static randomString(len: number): string;
+    /**
+     * 删除对象为空的属性
+     * @param obj 对象
+     */
+    static delEmptyAttr(obj: {
+        [index: string]: any;
+    }): {
+        [index: string]: any;
+    };
 }
 export default Core;
 //# sourceMappingURL=core.d.ts.map
--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@
  * @Description  :
  * @Autor        : Qzr(z5021996@vip.qq.com)
  * @LastEditors  : Qzr(z5021996@vip.qq.com)
- * @LastEditTime : 2022-05-05 14:31:48
+ * @LastEditTime : 2023-12-08 16:18:46
  */
 
 import resolve from 'rollup-plugin-node-resolve'
@@ -11,6 +11,8 @@ import commonjs from 'rollup-plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 import babel from '@rollup/plugin-babel'
 import pkg from './package.json'
+import terser from '@rollup/plugin-terser'
+
 
 export default {
   input: 'src/index.ts', // 打包入口
@@ -22,6 +24,7 @@ export default {
     resolve(), // 查找和打包node_modules中的第三方模块
     commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
     typescript(), // 解析TypeScript
-    babel()
+    babel(),
+    terser()
   ]
 }
